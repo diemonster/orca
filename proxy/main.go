c103c1b25fafe2ae1bc29dfe7602cbaca87f2cfe
Zack Patrick <zpatrick319@Hotmail.com>
2018-09-26 14:32:34 -0700

    add --local config option

--- proxy/main.go
+++ proxy/main.go
@@ -1,40 +1,82 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"net/http"
-	"net/http/httputil"
+	"os"
 
+	"github.com/urfave/cli"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/transport"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	FlagLocal = "local"
+)
+
+const (
+	EVLocal = "OP_LOCAL"
 )
 
 func main() {
-	// todo: flags to switch between in-cluster and local development? 
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		log.Fatal(err)
+	app := cli.NewApp()
+	app.Name = "orca-proxy"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:   FlagLocal,
+			EnvVar: EVLocal,
+		},
 	}
 
-	// https://github.com/kubernetes/client-go/blob/02384dbe123ff097a279965297f327a72ebefb72/transport/round_trippers.go#L100
-	// https://github.com/kubernetes/client-go/blob/02384dbe123ff097a279965297f327a72ebefb72/transport/transport.go
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		// todo: update config.Impersonate with data from request header
-		
-		rt, err := transport.New(config)
-		if err != nil {
-			log.Println(err.Error())
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+	app.Action = func(c *cli.Context) error {
+		getConfig := rest.InClusterConfig
+		if c.Bool(FlagLocal) {
+			getConfig = func() (*rest.Config, error) {
+				return clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
+			}
 		}
+
+		config, err := getConfig()
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(config)
+		return nil
 	}
 
-	rp := httputil.NewSingleHostReverseProxy("") // todo: config.BaseURL?
-
-	// whenever a request comes in:
-	rt, err := transport.HTTPWrappersForConfig(config, rt)
-	if err != nil {
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+	/*
+
+		// todo: flags to switch between in-cluster and local development?
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// https://github.com/kubernetes/client-go/blob/02384dbe123ff097a279965297f327a72ebefb72/transport/round_trippers.go#L100
+		// https://github.com/kubernetes/client-go/blob/02384dbe123ff097a279965297f327a72ebefb72/transport/transport.go
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			// todo: update config.Impersonate with data from request header
+
+			// todo: should this use transport.TLSConfigFor ?
+			rt, err := transport.New(config)
+			if err != nil {
+				log.Println(err.Error())
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+
+		rp := httputil.NewSingleHostReverseProxy("") // todo: config.BaseURL?
+
+		// whenever a request comes in:
+		rt, err := transport.HTTPWrappersForConfig(config, rt)
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
 
 }

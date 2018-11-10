096f3ee3c8b6a249eaaf8e4b87022268aaf1ef20
Tanner Lake <tanner.lake@gmail.com>
2018-11-09 16:38:23 -0800

    Add tests for NamespaceList component

---
+++ rostrum/src/components/namespaces/NamespaceList.test.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { NamespaceList } from './NamespaceList';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('components', () => {
+  describe('NamespaceList', () => {
+    it('should render itself and its subcomponents when namespaceObjects === 0', () => {
+      const props = {
+        dispatchNamespaceList: jest.fn(),
+        namespaceObjects: [],
+      };
+
+      const namespaceList = shallow(<NamespaceList {...props} />);
+
+      expect(namespaceList.find('div').hasClass('namespace-list-container')).toBe(true);
+      expect(namespaceList.find('h2').text()).toEqual('List Namespaces');
+      expect(namespaceList.find('p').text()).toEqual('Loading namespaces...');
+    });
+
+    it('should render itself and subcomponents when namespaceObjects.length > 0', () => {
+      const props = {
+        dispatchNamespaceList: jest.fn(),
+        namespaceObjects: [
+          { name: 'namespace1', status: 'Active' },
+          { name: 'namespace2', status: 'Terminating' },
+        ],
+      };
+
+      const namespaceList = shallow(<NamespaceList {...props} />);
+
+      expect(namespaceList.find('div').hasClass('namespace-list-container')).toBe(true);
+      expect(namespaceList.find('h2').text()).toEqual('List Namespaces');
+      expect(namespaceList.find('ul').hasClass('namespace-list')).toBe(true);
+
+      const listItems = namespaceList.find('ul').children();
+      expect(listItems.length).toEqual(2);
+      expect(listItems.at(0).props().namespace).toEqual('namespace1');
+      expect(listItems.at(0).props().phase).toEqual('Active');
+      expect(listItems.at(1).props().namespace).toEqual('namespace2');
+      expect(listItems.at(1).props().phase).toEqual('Terminating');
+    });
+  });
+});

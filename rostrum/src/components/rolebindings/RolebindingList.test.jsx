a2a3dc8126340f4c0042f0f0b47933815c9a2834
Tanner Lake <tanner.lake@gmail.com>
2018-11-12 13:27:32 -0800

    Add tests for Rolebinding components

---
+++ rostrum/src/components/rolebindings/RolebindingList.test.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import { RolebindingList } from './RolebindingList';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('components', () => {
+  describe('RolebindingList', () => {
+    it('should render itself and its subcomponents when namespacedRolebindings === 0', () => {
+      const props = {
+        dispatchRolebindingList: jest.fn(),
+        namespace: 'some-namespace',
+        namespacedRolebindings: {},
+      };
+
+      const rolebindingList = shallow(<RolebindingList {...props} />);
+
+      expect(rolebindingList.find('div').hasClass('rolebinding-container')).toBe(true);
+      expect(rolebindingList.find('h5').text()).toEqual('Role Bindings:');
+      expect(rolebindingList.find('ul').hasClass('rolebinding-list')).toBe(true);
+      expect(rolebindingList.find('ul').children().length).toEqual(0);
+    });
+
+    it('should render itself and subcomponents when namespacedRolebindings.length > 0 and no namespaces match', () => {
+      const props = {
+        dispatchRolebindingList: jest.fn(),
+        namespace: 'some-namespace',
+        namespacedRolebindings: {
+          'some-other-namespace': ['some-rolebinding'],
+        },
+      };
+
+      const rolebindingList = shallow(<RolebindingList {...props} />);
+
+      expect(rolebindingList.find('div').hasClass('rolebinding-container')).toBe(true);
+      expect(rolebindingList.find('h5').text()).toEqual('Role Bindings:');
+      expect(rolebindingList.find('ul').hasClass('rolebinding-list')).toBe(true);
+      expect(rolebindingList.find('ul').children().length).toEqual(0);
+    });
+
+    it('should render itself and subcomponents when namespacedRolebindings.length > 0 and a namespace matches', () => {
+      const props = {
+        dispatchRolebindingList: jest.fn(),
+        namespace: 'some-namespace',
+        namespacedRolebindings: {
+          'some-namespace': ['some-rolebinding', 'some-other-rolebinding'],
+        },
+      };
+
+      const rolebindingList = shallow(<RolebindingList {...props} />);
+
+      expect(rolebindingList.find('div').hasClass('rolebinding-container')).toBe(true);
+      expect(rolebindingList.find('h5').text()).toEqual('Role Bindings:');
+      const ul = rolebindingList.find('ul');
+      expect(ul.hasClass('rolebinding-list')).toBe(true);
+      expect(ul.children().length).toEqual(2);
+      expect(ul.childAt(0).prop('rolebindingName')).toEqual('some-rolebinding');
+      expect(ul.childAt(1).prop('rolebindingName')).toEqual('some-other-rolebinding');
+    });
+  });
+});
---
+++ rostrum/src/components/rolebindings/RolebindingListItem.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import RolebindingListItem from './RolebindingListItem';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('components', () => {
+  describe('RolebindingListItem', () => {
+    it('should render itself and its subcomponents', () => {
+      const props = {
+        rolebindingName: 'some-rolebinding',
+      };
+
+      const rolebindingListItem = shallow(<RolebindingListItem {...props} />);
+
+      expect(rolebindingListItem.find('li').hasClass('rolebinding-list-item')).toBe(true);
+      expect(rolebindingListItem.find('li').text()).toEqual('some-rolebinding');
+    });
+  });
+});

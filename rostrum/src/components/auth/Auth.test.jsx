e8287b941513c93264d511c882d284106a9fa356
Tanner Lake <tanner.lake@gmail.com>
2018-11-12 14:40:53 -0800

    Add tests for Auth component

---
+++ rostrum/src/components/auth/Auth.test.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import Auth from './Auth';
+import Authenticator from '../../authenticator/authenticator';
+import NavigationBar from '../navigationBar/NavigationBar';
+import App from '../App';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('components', () => {
+  describe('Auth', () => {
+    it('renders a login view if unauthenticated', () => {
+      const authenticator = new Authenticator('', '');
+      authenticator.login = jest.fn();
+      authenticator.isAuthenticated = jest.fn(() => false);
+      authenticator.getProfile = jest.fn((cb) => { cb(null, {}); });
+
+      const props = { authenticator };
+
+      const auth = shallow(<Auth {...props} />);
+
+      expect(auth.find('div').hasClass('login-container')).toBe(true);
+      expect(auth.find(NavigationBar).prop('login')).toBe(authenticator.login);
+      expect(auth.find('h1').text()).toEqual('ORCA');
+    });
+
+    it('renders a logout view if authenticated', () => {
+      const authenticator = new Authenticator('', '');
+      authenticator.logout = jest.fn();
+      authenticator.isAuthenticated = jest.fn(() => true);
+      authenticator.getProfile = jest.fn((cb) => {
+        cb(null, { name: 'username' });
+      });
+
+      const props = { authenticator };
+
+      const auth = shallow(<Auth {...props} />);
+
+      expect(auth.find('div').hasClass('home-container')).toBe(true);
+      expect(auth.find(NavigationBar).prop('name')).toBe('username');
+      expect(auth.find(NavigationBar).prop('logout')).toBe(authenticator.logout);
+      expect(auth.find(App).length).not.toEqual(0);
+    });
+  });
+});

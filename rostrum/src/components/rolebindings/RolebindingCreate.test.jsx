f7fc751600e6015acce96883202f897a8e46317e
Tanner Lake <tanner.lake@gmail.com>
2018-11-28 23:00:07 -0800

    Add tests for RolebindingCreate component

--- rostrum/src/components/rolebindings/RolebindingCreate.test.jsx
+++ rostrum/src/components/rolebindings/RolebindingCreate.test.jsx
@@ -8,8 +8,10 @@ Enzyme.configure({ adapter: new Adapter() });
 
 describe('components', () => {
   describe('RolebindingCreate', () => {
-    it('should render itself and its subcomponents', () => {
-      const props = {
+    let props;
+
+    beforeEach(() => {
+      props = {
         dispatchRolebindingCreate: jest.fn(),
         dispatchRolebindingCreateChangeInput: jest.fn(),
         rolebindingCreateRoleInput: '',
@@ -17,9 +19,41 @@ describe('components', () => {
         roleOptions: [],
         selectedNamespace: '',
       };
+    });
 
+    it('should render itself and its subcomponents', () => {
       const rolebindingCreate = shallow(<RolebindingCreate {...props} />);
       expect(rolebindingCreate).toMatchSnapshot();
+    });
+
+    it('dispatches an action to change input when a user enters text', () => {
+      const mockFn = jest.fn();
+      props.dispatchRolebindingCreateChangeInput = mockFn;
+
+      const rolebindingCreate = shallow(<RolebindingCreate {...props} />);
+
+      const event = {
+        preventDefault: jest.fn(),
+        target: { value: 'a', name: 'name' },
+      };
+
+      rolebindingCreate.find('input').simulate('change', event);
+      expect(mockFn).toBeCalledWith('name', 'a');
+    });
+
+    it('dispatches an action to create a rolebinding on form submit', () => {
+      const mockFn = jest.fn();
+      props.dispatchRolebindingCreate = mockFn;
+      props.rolebindingCreateRoleInput = 'some-role';
+      props.rolebindingCreateSubjectInput = 'some-subject';
+      props.selectedNamespace = 'some-namespace';
+
+      const rolebindingCreate = shallow(<RolebindingCreate {...props} />);
+
+      const event = { preventDefault: jest.fn() };
+
+      rolebindingCreate.find('form').simulate('submit', event);
+      expect(mockFn).toBeCalledWith('some-namespace', 'some-role', 'some-subject');
     });
   });
 });
